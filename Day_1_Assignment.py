b41d492b9189a0afbee601316b279779a2ba96b4
Shilpa V <57183030+shilpav14@users.noreply.github.com>
2020-11-08 12:31:24 +0530

    Add files via upload

---
+++ Day_1_Assignment.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+print("RAINBOW")
+
+
+# In[ ]:
+
+
+print("LETS UPGRADE")
+
+
+# In[ ]:
+
+
+cp = int(input())
+sp = int(input())
+if (sp > cp):
+    print("Profit")
+elif (cp > sp):
+    print("Loss")
+else:
+    print("Neither")
+
+
+# In[ ]:
+
+
+euro = int(input())
+rs = euro*80
+print(rs)
+
